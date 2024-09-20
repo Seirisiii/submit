eec4dcd707e14d68b6caa7fade1dae0629e6a7ed
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-09-21 06:15:37 +0700

    0007 commit

---
+++ programming/0007.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int r;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>r;
+    cout<<fixed<<setprecision(6)<<M_PI*r*r<<"\n"<<r*r*2.0;
+    return 0;
+}
