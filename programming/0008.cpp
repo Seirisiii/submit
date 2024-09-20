6a05a41ddd6fba07306c473e3be0fd7f850da07f
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-09-21 06:16:09 +0700

    0008 commit

---
+++ programming/0008.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int x1,s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>x1>>s;
+    cout<<s*2-x1;
+    return 0;
+}
