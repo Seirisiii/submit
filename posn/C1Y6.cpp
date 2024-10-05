de386482a02d3280d665f4eb5207349511ca7059
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-10-05 20:22:24 +0700

    C1Y6 commit

---
+++ posn/C1Y6.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a;
+    if(a%2)cout<<"Odd";
+    else cout<<"Even";
+    return 0;
+}
