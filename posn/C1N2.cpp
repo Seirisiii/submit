a2a3313f9498fcf9f62f3460edfe24dca8c76b82
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-10-05 20:12:35 +0700

    C1N2 commit

---
+++ posn/C1N2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+double a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    cout<<fixed<<setprecision(2)<<a<<"\n"<<b<<"\n"<<a/b/b;
+    return 0;
+}
