f139cbc14e3157806bba04d301f04b9c00a62e73
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-10-07 04:10:49 +0700

    C1N19 commit

---
+++ posn/C1N19.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+char s[20];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    scanf("%s",s);
+    printf("%d",strlen(s));
+    return 0;
+}
