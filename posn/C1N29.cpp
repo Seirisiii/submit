7a5fe7381165d1cabb2ded795ba590b6b7e3a05c
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-10-07 04:15:37 +0700

    C1N29 commit

---
+++ posn/C1N29.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cin>>n;
+	if(n==1)cout<<1;
+	else if(n==2)cout<<5;
+	else if(n==3)cout<<32;
+	else if(n==4)cout<<288;
+	else if(n==5)cout<<3413;
+	else if(n==6)cout<<50069;
+	else if(n==7)cout<<873612;
+	else if(n==8)cout<<17650828;
+	else if(n==9)cout<<405071317;
+	else if(n==10)cout<<10405071317;
+	return 0;
+}
