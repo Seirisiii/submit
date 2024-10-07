8d01c944e611c31d4e6d568175c8e5d413ef6450
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-10-07 21:53:36 +0700

    C1T28 commit

---
+++ posn/C1T28.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+char c1,c2,c3,c4;
+int main(){
+     cin>>n;
+     cin>>c1>>c2>>c3>>c4;
+     for(int i=0;i<n;++i){
+          for(int j=0;j<n;++j){
+               if(j>n-i-1)cout<<c3<<" ";
+               else cout<<c1<<" ";
+          }
+          cout<<" ";
+          for(int j=0;j<n;++j){
+               if(j>=i)cout<<c2<<" ";
+               else cout<<c4<<" ";
+          }
+          cout<<"\n";
+     }
+     cout<<"\n";
+     for(int i=0;i<n;++i){
+          for(int j=0;j<n;++j){
+               if(j>i)cout<<c4<<" ";
+               else cout<<c2<<" ";
+          }
+          cout<<" ";
+          for(int j=0;j<n;++j){
+               if(j>=n-i-1)cout<<c1<<" ";
+               else cout<<c3<<" ";
+          }
+          cout<<"\n";
+     }
+     return 0;
+}
