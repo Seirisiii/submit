61c42c82063a2a5144423d8e8fb02a961e68891f
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-10-08 18:07:08 +0700

    C1T21 save comit

--- posn/C1T21.cpp
+++ posn/C1T21.cpp
@@ -1,80 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-int roll[105],box[105][105];
-int mxhigh[105];
-int bighigh=INT_MIN;
-int mxroll[105];
-int bigroll;
-int mid[105][105];
-int high[105][105];
-int I[105];
-int l[105][105];
-int r[105][105];
-int c[105][105];
+int n;
+int m[100];
+int box[100][100];
+int h[100],w[100];
+int mxi,mxj;
+int mid[100];
 int main(){
-    int n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin>>n;
-    for(int i=1;i<=n;++i){
-        cin>>roll[i];
-        for(int j=1;j<=roll[i];++j){
+    for(int i=0;i<n;++i){
+        cin>>m[i];
+        for(int j=0;j<m[i];++j){
             cin>>box[i][j];
-            high[i][j]=box[i][j]+2;
+            h[i]+=box[i][j]+2;
+            w[i]=max(box[i][j]*2+3,w[i]);
         }
+        mid[i]=(mxj+mxj+w[i])/2;
+        mxi=max(h[i],mxi);
+        mxj+=w[i];
     }
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=roll[i];++j){
-            mxhigh[i]+=box[i][j]+2;
-            mxroll[i]=mxroll[i]>box[i][j]*2+3?mxroll[i]:box[i][j]*2+3;
-        }
-        bighigh=bighigh>mxhigh[i]?bighigh:mxhigh[i];
-        bigroll+=mxroll[i];
-    }
-    for(int i=1;i<=n;++i){
-        mxroll[i]+=mxroll[i-1];
-    }
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=roll[i];++j){
-            high[i][j]+=high[i][j-1];
-        }
-    }
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=roll[i];++j){
-            mid[i][j]=(mxroll[i]-mxroll[i-1]-(box[i][j]*2+3))/2+2+mxroll[i-1]+box[i][j];
-            l[i][j]=mid[i][j];
-            r[i][j]=mid[i][j];
-        }
-    }
-    for(int i=1;i<=n;++i){
-        I[i]=roll[i];
-    }
-    for(int i=bighigh;i>0;--i){
-        int J=1;
-        for(int j=1;j<=bigroll;++j){
-            if(j>mxroll[J])++J;
-            if(i<=high[J][I[J]]-box[J][I[J]]-2)--I[J];
-            if(i>high[J][I[J]])cout<<" ";
-            else if(i==high[J][I[J]]){
-                if(j>mid[J][I[J]]-box[J][I[J]]-1 and j<mid[J][I[J]]+box[J][I[J]]+1)cout<<"_";
-                else cout<<" ";
+    char arr[mxi+1][mxj+1];
+    memset(arr,' ',sizeof(arr));
+    for(int i=0;i<n;++i){
+        int y=mxi;
+        for(int j=0;j<m[i];++j){
+            char c='A';
+            y-=box[i][j]+2;
+            arr[y+1][mid[i]]='^';
+            for(int a=mid[i]-box[i][j];a<=mid[i]+box[i][j];++a){
+                arr[y][a]='_';
             }
-            else if(i<high[J][I[J]]){
-                if(j==mid[J][I[J]]-box[J][I[J]]-1 or j==mid[J][I[J]]+box[J][I[J]]+1)cout<<"|";
-                else if(j==r[J][I[J]] and j==l[J][I[J]])cout<<"^";
-                else if(j==l[J][I[J]])cout<<"/";
-                else if(j==r[J][I[J]])cout<<"\\";
-                else if(j>l[J][I[J]] and j<r[J][I[J]]){
-                        printf("%c",c[J][I[J]]%26+'A');
-                        ++c[J][I[J]];
+            for(int a=y+1;a<=y+box[i][j]+1;++a){
+                arr[a][mid[i]-box[i][j]-1]='|';
+                arr[a][mid[i]+box[i][j]+1]='|';
+            }
+            int l=mid[i]-1;
+            int r=mid[i]+1;
+            for(int a=y+2;a<=y+box[i][j]+1;++a){
+                for(int b=l;b<=r;++b){
+                    if(b==l)arr[a][b]='/';
+                    if(b==r)arr[a][b]='\\';
+                    else if(b>l and b<r){
+                        arr[a][b]=c;
+                        ++c;
+                        if(c>'Z')c='A';
+                    }
                 }
-                else cout<<" ";
+                --l;
+                ++r;
             }
-            else cout<<" ";
         }
-        for(int j=1;j<=n;++j){
-            if(i<high[j][I[j]]){
-                --l[j][I[j]];
-                ++r[j][I[j]];
-            }
+    }
+    for(int i=0;i<mxi;++i){
+        for(int j=0;j<mxj;++j){
+            cout<<arr[i][j];
         }
         cout<<"\n";
     }
