b16864b3ae59a71628b15b3854b40126eb8a899a
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2024-12-21 15:44:53 +0700

    Modify 1090 commit

--- programming/1090.cpp
+++ programming/1090.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int n;
 int a;
-long long cnt[200000];
+long long cnt[100001];
 long long res;
 int main(){
     ios_base::sync_with_stdio(false);
@@ -14,7 +14,7 @@ int main(){
         ++cnt[x];
     }
     cin>>a;
-    for(int i=0;i<=min(100000,a/2);++i){
+    for(int i=max(0,a-100000);i<=min(100000,a/2);++i){
         if(i==a-i)res+=cnt[i]*(cnt[i]-1)/2;
         else res+=cnt[i]*cnt[a-i];
     }
