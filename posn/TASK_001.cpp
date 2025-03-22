4b91c0339b740d5293cab031f8e45f8a1da66195
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2025-03-22 19:47:12 +0700

    TASK_001 commit

---
+++ posn/TASK_001.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    cout<<a*b;
+    return 0;
+}
