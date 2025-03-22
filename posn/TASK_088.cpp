fc759a28b17cf878afad16681086a40772b9581d
Thitikorn Kham-ai <thitikorn.tk2550@gmail.com>
2025-03-22 19:54:29 +0700

    TASK_088 commit

---
+++ posn/TASK_088.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e6+5;
+const int MOD=1e9+7;
+int n;
+long long dp[N];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    dp[0]=1;
+    dp[1]=1;
+    dp[2]=1;
+    dp[3]=2;
+    for(int i=4;i<=n;++i){
+        dp[i]=(dp[i-1]+dp[i-3]+dp[i-4])%MOD;
+    }
+    cout<<dp[n];
+    return 0;
+}
